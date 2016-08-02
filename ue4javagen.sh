2ac25103ea84d8c861891ede0f7566d0180630bd
Robert Segal <rob@getsetgames.com>
2016-08-01 22:31:31 -0400

    Will print out class method signatures and starting to mess with filtering output from javap since not all the output is needed

--- ue4javagen.sh
+++ ue4javagen.sh
@@ -4,13 +4,29 @@ process() {
 	#echo $*
 	#jdeps $*
 
+	echo "Searching JAR $*..." 
+
 	for f in $(jar tf $*)
 	do
 		# Find clasess defined JAR files
 		#
 		if [[ $f =~ ".class" ]]
 		then
-			echo $f
+			javap -s -p -classpath "$*" $(echo $f | tr / . | sed s/.class//g) | egrep 'Compiled from|descriptor|public|private|\(|\)'
+			
+			# JNI Call
+			#echo "if (JNIEnv* Env = FAndroidApplication::GetJavaEnv())"
+    # {
+    #     static jmethodID Method = FJavaWrapper::FindMethod(Env,
+    #                                                        FJavaWrapper::GameActivityClassID,
+    #                                                        "AndroidThunkJava_Module",
+    #                                                        "()Ljava/lang/String;[java/lang/String;[java/lang/String;",
+    #                                                        false);
+        
+    #     FJavaWrapper::CallObjectMethod(Env, FJavaWrapper::GameActivityThis, Method);
+    # }			
+
+			# Java Call
 		fi
 	done
 }
@@ -18,3 +34,7 @@ process() {
 export -f process
 
 find "$1" -name *jar -exec bash -c 'process "$0"' {} \;
+
+
+# extern "C" void Java_com_epicgames_ue4_GameActivity_nativeResumeMainInit(JNIEnv* jenv, jobject thiz)
+# {
