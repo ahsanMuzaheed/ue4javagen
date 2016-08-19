9b2e6d43981000a15c339e7918f50ec46f96ee35
Robert Segal <rob@getsetgames.com>
2016-08-19 12:23:04 -0400

    Gets rid of commented code

--- ue4javagen.sh
+++ ue4javagen.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 process() {
-	#echo $*
-	#jdeps $*
-
 	echo "Searching JAR $*..." 
 
 	for f in $(jar tf $*)
@@ -13,20 +10,6 @@ process() {
 		if [[ $f =~ ".class" ]]
 		then
 			javap -s -p -classpath "$*" $(echo $f | tr / . | sed s/.class//g) | egrep 'Compiled from|descriptor|public|private|\(|\)'
-			
-			# JNI Call
-			#echo "if (JNIEnv* Env = FAndroidApplication::GetJavaEnv())"
-    # {
-    #     static jmethodID Method = FJavaWrapper::FindMethod(Env,
-    #                                                        FJavaWrapper::GameActivityClassID,
-    #                                                        "AndroidThunkJava_Module",
-    #                                                        "()Ljava/lang/String;[java/lang/String;[java/lang/String;",
-    #                                                        false);
-        
-    #     FJavaWrapper::CallObjectMethod(Env, FJavaWrapper::GameActivityThis, Method);
-    # }			
-
-			# Java Call
 		fi
 	done
 }
@@ -35,6 +18,3 @@ export -f process
 
 find "$1" -name *jar -exec bash -c 'process "$0"' {} \;
 
-
-# extern "C" void Java_com_epicgames_ue4_GameActivity_nativeResumeMainInit(JNIEnv* jenv, jobject thiz)
-# {
